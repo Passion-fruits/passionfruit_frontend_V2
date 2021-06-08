c34cb3148e8576643895a1c2ad140078beff57f6
jiwon <jidole041214@naver.com>
2021-06-08 10:03:15 +0900

    [ADD] SVG

---
+++ src/assets/search.tsx
@@ -0,0 +1,18 @@
+export default function Search() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13.884"
+      height="13.887"
+      viewBox="0 0 13.884 13.887"
+    >
+      <path
+        id="Icon_ionic-ios-search"
+        data-name="Icon ionic-ios-search"
+        d="M18.221,17.375l-3.861-3.9a5.5,5.5,0,1,0-.835.846L17.36,18.2a.594.594,0,0,0,.839.022A.6.6,0,0,0,18.221,17.375Zm-8.186-3A4.345,4.345,0,1,1,13.109,13.1,4.318,4.318,0,0,1,10.035,14.371Z"
+        transform="translate(-4.5 -4.493)"
+        fill="#686868"
+      />
+    </svg>
+  );
+}
