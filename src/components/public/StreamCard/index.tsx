55adc7d405e1e3f491333e31ad680900b4a22cbe
Sung Ye In <syi9397@naver.com>
2021-06-09 08:24:57 +0900

    [UPDATE] styled structure

--- src/components/public/StreamCard/index.tsx
+++ src/components/public/StreamCard/index.tsx
@@ -1,1 +1,15 @@
 import * as S from "./styles";
+
+function StreamCard(props) {
+    const {} = props;
+
+    return (
+        <S.Wrapper>
+            <S.CoverImg />
+            <S.Title></S.Title>
+            <S.UserName></S.UserName>
+        </S.Wrapper>
+    );
+}
+
+export default StreamCard;
--- src/components/public/StreamCard/styles.ts
+++ src/components/public/StreamCard/styles.ts
@@ -1,1 +1,34 @@
 import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+    width: 220px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+export const CoverImg = styled.img`
+    width: 100%;
+    height: 220px;
+    object-fit: cover;
+`;
+
+export const Title = styled.h3`
+    color: white;
+    font-size: 17px;
+    font-weight: bold;
+`;
+
+export const UserName = styled.span`
+    color: #929292;
+    font-size: 12px;
+    font-weight: normal;
+`;
+
+export const Bottom = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
