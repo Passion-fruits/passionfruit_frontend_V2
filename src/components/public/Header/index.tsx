33d569e21f657b5ac05c70805640e5baca289b96
jiwon <jidole041214@naver.com>
2021-06-08 10:04:04 +0900

    [CREATE] header
    퍼블리싱만 된 상태

--- src/components/public/Header/index.tsx
+++ src/components/public/Header/index.tsx
@@ -1,1 +1,27 @@
+import HambergerMenu from "@src/assets/hambergerMenu";
+import Logo from "@src/assets/logo";
+import Search from "@src/assets/search";
 import * as S from "./styles";
+
+export default function Header() {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <Logo />
+        <S.SearchBar>
+            <Search/>
+            <S.Input placeholder="정보를 찾아드릴게요"/>
+            <S.Select>
+                <option value="">All</option>
+                <option value="">Track</option>
+                <option value="">User</option>
+                <option value="">PlayList</option>
+            </S.Select>
+        </S.SearchBar>
+      </S.Container>
+      <S.Container>
+          <HambergerMenu/>
+      </S.Container>
+    </S.Wrapper>
+  );
+}
--- src/components/public/Header/styles.ts
+++ src/components/public/Header/styles.ts
@@ -1,1 +1,58 @@
 import styled from "@emotion/styled";
+
+const BACKGROUND_COLOR = "#161616";
+
+const BORDER = "#2C2C2C";
+
+const PADDING = "225px";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100px;
+  background: ${BACKGROUND_COLOR};
+  border-bottom: 1px solid ${BORDER};
+  padding: 0 ${PADDING};
+  display: flex;
+  align-items: center;
+  color: white;
+  justify-content: space-between;
+  position: fixed;
+`;
+
+export const SearchBar = styled.div`
+  width: 320px;
+  height: 35px;
+  background: #2d2929;
+  border-radius: 24px;
+  margin-left: 25px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 80%;
+  color: #919191;
+  font-weight: 500;
+  font-size: 13px;
+  padding: 0 10px;
+  ::placeholder {
+    font-weight: 600;
+    color: #686868;
+  }
+`;
+
+export const Select = styled.select`
+  background: none;
+  border: none;
+  color: gray;
+  font-size: 13px;
+  outline: none;
+  text-align-last: right;
+  padding: 0 5px;
+`;
