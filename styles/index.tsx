cf0939ee92dfef57b72572734d2917a76ea996a1
jiwon <jidole041214@naver.com>
2021-06-09 16:44:47 +0900

    [CREATE] style index file

---
+++ styles/index.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const ImgWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+`;
+
+export const CoverImg = styled.img`
+  width: 100%;
+  position: absolute;
+  object-fit: cover;
+  height: 100%;
+`;
+
+export const ProfileImg = styled(CoverImg)`
+border-radius:80%;
+box-shadow:0px 0px 20px rgb(10,10,10);
+`
