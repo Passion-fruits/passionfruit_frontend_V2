219bb39fe51faa1104611afe6533f512eb5aae19
Sung Ye In <syi9397@naver.com>
2021-06-15 16:56:25 +0900

    [UPDATE] tag component
    tag is tags wrapper

--- src/components/public/Tag/index.tsx
+++ src/components/public/Tag/index.tsx
@@ -1,1 +1,14 @@
 import * as S from "./styles";
+
+function Tag(props) {
+    const { tags } = props;
+    return (
+        <S.Wrapper>
+            {tags.map((tag) => (
+                <S.Tag># {tag}</S.Tag>
+            ))}
+        </S.Wrapper>
+    );
+}
+
+export default Tag;
