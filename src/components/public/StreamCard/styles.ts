ad937422c831190233af9cb46de2cf17dbfbf1d9
jiwon <jidole041214@naver.com>
2021-06-20 21:58:43 +0900

    [UPDATE] play button style

--- src/components/public/StreamCard/styles.ts
+++ src/components/public/StreamCard/styles.ts
@@ -2,71 +2,75 @@ import styled from "@emotion/styled";
 import { CARD_WIDTH } from "styles";
 
 export const Wrapper = styled.div`
-    width: ${CARD_WIDTH};
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+  width: ${CARD_WIDTH};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 export const CoverImgHover = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.2);
-    visibility: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.2);
+  visibility: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ImgWrapper = styled.div`
-    width: 100%;
-    height: ${CARD_WIDTH};
-    position: relative;
-    cursor: pointer;
+  width: 100%;
+  height: ${CARD_WIDTH};
+  position: relative;
+  cursor: pointer;
 
-    &:hover div {
-        visibility: visible;
-    }
+  &:hover div {
+    visibility: visible;
+  }
 `;
 
 export const PlayButton = styled.button`
-    width: 56px;
-    height: 56px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    border: 2px solid white;
-    background:rgb(0,0,0,0.4);
-    cursor: pointer;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background: rgb(0, 0, 0, 0.4);
+  cursor: pointer;
+  &:hover {
+    width: 60px;
+    height: 60px;
+  }
 `;
 
 export const CoverImg = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 export const Title = styled.h3`
-    color: white;
-    font-size: 17px;
-    font-weight: bold;
-    margin-bottom:7px;
+  color: white;
+  font-size: 17px;
+  font-weight: bold;
+  margin-bottom: 7px;
 `;
 
 export const UserName = styled.span`
-    color: #929292;
-    font-size: 12px;
-    font-weight: normal;
+  color: #929292;
+  font-size: 12px;
+  font-weight: normal;
 `;
 
 export const Bottom = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
