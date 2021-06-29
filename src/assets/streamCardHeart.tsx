1d57859c59a36605c127a7283b37317c6e246904
jiwon <jidole041214@naver.com>
2021-06-29 20:00:48 +0900

    [ADD] stream card icon

---
+++ src/assets/streamCardHeart.tsx
@@ -0,0 +1,22 @@
+export default function StreamCardHeart() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15.813"
+      height="13.409"
+      viewBox="0 0 20.813 18.409"
+    >
+      <path
+        id="Icon_feather-heart"
+        data-name="Icon feather-heart"
+        d="M19.685,5.948a4.95,4.95,0,0,0-7,0l-.954.954-.954-.954a4.951,4.951,0,1,0-7,7l.954.954,7,7,7-7,.954-.954a4.95,4.95,0,0,0,0-7Z"
+        transform="translate(-1.323 -3.497)"
+        fill="none"
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
