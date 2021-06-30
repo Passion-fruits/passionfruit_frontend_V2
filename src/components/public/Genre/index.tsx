7e5993dcf7425526dc26597a17de426671a01803
jiwon <jidole041214@naver.com>
2021-06-30 19:39:15 +0900

    [FIX] input radio checked error

--- src/components/public/Genre/index.tsx
+++ src/components/public/Genre/index.tsx
@@ -1,8 +1,13 @@
 import * as S from "./styles";
 import { GenreKinds } from "./../../../libs/exportFile/genre";
 import { GenreArr } from "@src/libs/interfaces/main";
+import { useState } from "react";
 
 export default function Genre() {
+  const [genre, setGenre] = useState<string>(GenreKinds[0].title);
+  const chooseGenre = (e): void => {
+    setGenre(e.target.value);
+  };
   return (
     <S.GenreBox>
       <h3>장르</h3>
@@ -10,11 +15,13 @@ export default function Genre() {
       {GenreKinds.map((e: GenreArr, index: number) => (
         <S.Select key={index}>
           <S.CHECK_BOX>
-            {index === 0 ? (
-              <input checked type="radio" value={e.title} name="genre" />
-            ) : (
-              <input type="radio" value={e.title} name="genre" />
-            )}
+            <input
+              type="radio"
+              value={e.title}
+              name="genre"
+              onChange={chooseGenre}
+              checked={e.title === genre}
+            />
             <label>{e.title}</label>
           </S.CHECK_BOX>
         </S.Select>
@@ -25,15 +32,18 @@ export default function Genre() {
       {GenreKinds.map((e: GenreArr, index: number) => (
         <S.Select key={index}>
           <S.CHECK_BOX>
-            {index === 0 ? (
-              <input checked type="radio" value={e.title} name="sort" />
-            ) : (
-              <input type="radio" value={e.title} name="sort" />
-            )}
+            <input
+              type="radio"
+              value={e.title}
+              name="sort"
+              onChange={chooseGenre}
+              checked={e.title === genre}
+            />
             <label>{e.title}</label>
           </S.CHECK_BOX>
         </S.Select>
       ))}
+
     </S.GenreBox>
   );
 }
