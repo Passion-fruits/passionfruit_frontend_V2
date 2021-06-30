7fa0e70f6d7975ef19bd395b506fbce854455cd6
jiwon <jidole041214@naver.com>
2021-06-30 11:13:29 +0900

    [UPDATE] routing change check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import { PlayBar } from "@src/components/public";
 import { SampleProvider } from "@src/libs/context";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
   return (
     <SampleProvider>
       <PlayBar />
