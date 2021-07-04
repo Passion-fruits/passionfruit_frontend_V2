c0d3774cd67b1b39190498d8e56786082bc7a6a1
jiwon <jidole041214@naver.com>
2021-07-04 23:01:36 +0900

    [UPDATE] publishing

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -12,29 +12,52 @@ export default function Upload() {
         {/* 커버사진 업로드 */}
         <S.CoverPhotoContainer>
           <img
-            src="https://blog.kakaocdn.net/dn/0mySg/btqCUccOGVk/nQ68nZiNKoIEGNJkooELF1/img.jpg"
+            src="https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
             alt=""
           />
-          <S.UPLOAD_BTN width="100%" margin="28px">
-            Upload Cover
+          <S.UPLOAD_BTN width="100%" margin="20px">
+            커버사진 변경
           </S.UPLOAD_BTN>
         </S.CoverPhotoContainer>
         {/* 곡 정보 입력창 */}
         <S.InputsContainer>
-            
-          <S.InpTitle>file (mp3)</S.InpTitle>
+          <S.InpTitle style={{ marginTop: 0 }}>file (mp3)</S.InpTitle>
           <S.FileUpload>
             <input placeholder="업로드한 파일이 없습니다." />
             <S.UPLOAD_BTN width="50px">
               <FileUpload />
             </S.UPLOAD_BTN>
           </S.FileUpload>
-          
           <S.InpTitle>title</S.InpTitle>
           <input placeholder="곡 제목을 입력해주세요." />
-
-          <S.InpTitle>title</S.InpTitle>
-          <textarea placeholder="곡 제목을 입력해주세요." />
+          <S.InpTitle>Description</S.InpTitle>
+          <textarea placeholder="곡에 대한 설명을 적어주세요." />
+          {/* 장르 셀렉트 */}
+          <S.SelectContainer>
+            <div>
+              <S.InpTitle>Genre</S.InpTitle>
+              <S.Select>
+                <option value="">힙합</option>
+                <option value="">브루스</option>
+                <option value="">기타등등...</option>
+              </S.Select>
+            </div>
+            <div>
+              <S.InpTitle>Feeling</S.InpTitle>
+              <S.Select>
+                <option value="">힙합</option>
+                <option value="">브루스</option>
+                <option value="">기타등등...</option>
+              </S.Select>
+            </div>
+          </S.SelectContainer>
+          <S.UPLOAD_BTN
+            width="100%"
+            margin="30px"
+            style={{ padding: "11px 0", fontSize: "17px" }}
+          >
+            음악 공개하기
+          </S.UPLOAD_BTN>
         </S.InputsContainer>
       </S.Container>
     </S.Wrapper>
--- src/components/Upload/styles.ts
+++ src/components/Upload/styles.ts
@@ -1,20 +1,25 @@
 import styled from "@emotion/styled";
 import { COLOR, HEADER_HEIGHT } from "styles";
 
-interface UPLOAD_BTN_PROPS{
-    width:string;
-    margin?:string;
+interface UPLOAD_BTN_PROPS {
+  width: string;
+  margin?: string;
 }
 
 export const UPLOAD_BTN = styled.button<UPLOAD_BTN_PROPS>`
   background: ${COLOR.blue};
   color: white;
-  font-weight:bold;
+  font-size:15px;
+  font-weight: 600;
   padding: 7px 0;
-  width:${(e)=>e.width};
-  border-radius:3px;
-  margin-top:${(e)=>e.margin};
+  width: ${(e) => e.width};
+  border-radius: 3px;
+  margin-top: ${(e) => e.margin};
   cursor: pointer;
+  transition:0.4s;
+  &:hover{
+    opacity:0.8;
+  }
 `;
 
 export const Wrapper = styled.div`
@@ -25,7 +30,7 @@ export const Wrapper = styled.div`
 `;
 
 export const Container = styled.div`
-  padding-top: 60px;
+  padding-top: 40px;
   width: 950px;
   display: grid;
   color: white;
@@ -33,18 +38,20 @@ export const Container = styled.div`
     "description description"
     "coverPhoto inputs";
   grid-row-gap: 40px;
-  grid-column-gap:40px;
+  grid-column-gap: 40px;
 `;
 
 export const Description = styled.div`
   width: 100%;
   height: 30px;
   border-left: 2px solid ${COLOR.green};
-  padding: 10px 15px;
+  padding: 11px 15px;
   color: white;
   font-size: 17px;
   font-weight: bold;
   grid-area: description;
+  display:flex;
+  align-items:center;
   & b {
     color: ${COLOR.green};
   }
@@ -53,7 +60,6 @@ export const Description = styled.div`
 export const CoverPhotoContainer = styled.div`
   width: 260px;
   height: 30px;
-  background: grey;
   grid-area: coverPhoto;
   display: flex;
   flex-direction: column;
@@ -66,45 +72,62 @@ export const CoverPhotoContainer = styled.div`
 export const InputsContainer = styled.div`
   height: 30px;
   grid-area: inputs;
-  width:650px;
-  & input,textarea{
-      background:#303030;
-      color:#C2C2C2;
-      border-radius:3px;
-      border:1px solid #545454;
-      padding:13px 20px;
-      font-size:15px;
-      width:100%;
-      &::placeholder{
-      color:#C2C2C2;
-      }
+  width: 650px;
+  & input,
+  textarea {
+    background: #303030;
+    color: #c2c2c2;
+    border-radius: 3px;
+    border: 1px solid #545454;
+    padding: 13px 20px;
+    font-size: 15px;
+    width: 100%;
+    &::placeholder {
+      color: #c2c2c2;
+    }
   }
-  & textarea{
-      height:190px;
-      resize:none;
-      outline:none;
+  & textarea {
+    height: 190px;
+    resize: none;
+    outline: none;
   }
 `;
 
 export const FileUpload = styled.div`
-display:flex;
-justify-content:space-between;
-& button{
-    height:100%;
+  display: flex;
+  justify-content: space-between;
+  & button {
+    height: 100%;
   }
-  & input{
-      width:580px;
+  & input {
+    width: 580px;
   }
-`
+`;
 
 export const InpTitle = styled.div`
-color:white;
-font-weight:bold;
-text-transform:capitalize;
-font-size:17px;
-margin-top:20px;
-margin-bottom:17px;
-:first-of-type{
-    margin-top:0;
-}
-`
+  color: white;
+  font-weight: bold;
+  text-transform: capitalize;
+  font-size: 17px;
+  margin-top: 20px;
+  margin-bottom: 17px;
+`;
+
+export const SelectContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 5%;
+`;
+
+export const Select = styled.select`
+  background: #303030;
+  color: #c2c2c2;
+  border-radius: 3px;
+  border: 1px solid ${COLOR.green};
+  padding: 13px 20px;
+  font-size: 15px;
+  width: 100%;
+  outline:none;
+  color:${COLOR.green};
+`;
