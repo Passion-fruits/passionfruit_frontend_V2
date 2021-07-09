dac248fe1adb0ecbb94e7ec2826d19b0ea86f891
jiwon <jidole041214@naver.com>
2021-07-09 17:04:43 +0900

    [UPDATE]

--- src/libs/api/upload.ts
+++ src/libs/api/upload.ts
@@ -20,14 +20,5 @@ export default {
             },
             data : fd
         })
-    },
-    getMyMusic(){
-        return request({
-            url : '/api/song',
-            method:'get',
-            headers:{
-                "Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MjUxMjMxMjMxMn0.d5_ym4ogK1nj2sNmBJU447QFQ_1mvW1eGZ4JGAg42sY`
-            }
-        })
     }
 }
