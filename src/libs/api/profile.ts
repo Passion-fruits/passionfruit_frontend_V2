4e4602a43e2b7794313a2e37750d8238f38762b4
jiwon <jidole041214@naver.com>
2021-07-12 17:03:03 +0900

    [UPDATE]

--- src/libs/api/profile.ts
+++ src/libs/api/profile.ts
@@ -1,4 +1,5 @@
 import request from './axios/axios'
+import { TOKEN } from './export'
 
 export default {
     getMyMusic(){
@@ -6,7 +7,7 @@ export default {
             url : '/api/song',
             method:'get',
             headers:{
-                "Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MjUxMjMxMjMxMn0.d5_ym4ogK1nj2sNmBJU447QFQ_1mvW1eGZ4JGAg42sY`
+                "Authorization" : `Bearer ${TOKEN}`
             }
         })
     }
--- src/libs/api/upload.ts
+++ src/libs/api/upload.ts
@@ -1,5 +1,6 @@
 import request from './axios/axios'
 import { MusicObj } from './../interfaces/upload';
+import { TOKEN } from './export';
  
 export default {
     uploadMusic(musicObj : MusicObj){
@@ -16,7 +17,7 @@ export default {
             method:'post',
             headers:{
                 "Content-Type": "multipart/form-data",
-                "Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE1MTYyMzkwMjIsImV4cCI6MjUxMjMxMjMxMn0.d5_ym4ogK1nj2sNmBJU447QFQ_1mvW1eGZ4JGAg42sY`
+                "Authorization" : `Bearer ${TOKEN}`
             },
             data : fd
         })
